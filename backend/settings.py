28b650f764ad53a5d23b478f671bfb69f1d5cc77
shota-nukumizu <author2000.1225@gmail.com>
2022-06-13 16:30:00 +0900

    change database and failed migration

--- backend/settings.py
+++ backend/settings.py
@@ -76,11 +76,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sampledb',
+        'USER': 'author',
+        'PASSWORD': '@Python2000',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
